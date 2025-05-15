9f3d246a1c241ee703399d42c25e74da92077d1c
João Renato Mendes <joao.mendes@mindminers.com>
2025-05-14 21:50:11 -0300

    refactor: removed sensitive info

--- terraform/iam_users/main.tf
+++ terraform/iam_users/main.tf
@@ -14,8 +14,8 @@ resource "aws_iam_user_policy" "s3_policy" {
         Effect = "Allow",
         Action = "s3:*",
         Resource = [
-          "arn:aws:s3:::reddit-pipeline-bckt",
-          "arn:aws:s3:::reddit-pipeline-bckt/*"
+          "arn:aws:s3:::${var.bucket_name}",
+          "arn:aws:s3:::${var.bucket_name}/*"
         ]
       }
     ]
@@ -24,91 +24,4 @@ resource "aws_iam_user_policy" "s3_policy" {
 
 resource "aws_iam_access_key" "s3_key" {
   user = aws_iam_user.s3_user.name
-}
-
-# --- Glue User ---
-resource "aws_iam_user" "glue_user" {
-  name = "service-user-glue"
-}
-
-resource "aws_iam_user_policy" "glue_policy" {
-  name = "glue-service-policy"
-  user = aws_iam_user.glue_user.name
-
-  policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Effect = "Allow",
-        Action = [
-          "glue:*",
-          "logs:*",
-          "s3:GetObject",
-          "s3:PutObject"
-        ],
-        Resource = "*"
-      }
-    ]
-  })
-}
-
-resource "aws_iam_access_key" "glue_key" {
-  user = aws_iam_user.glue_user.name
-}
-
-# --- Athena User ---
-resource "aws_iam_user" "athena_user" {
-  name = "service-user-athena"
-}
-
-resource "aws_iam_user_policy" "athena_policy" {
-  name = "athena-service-policy"
-  user = aws_iam_user.athena_user.name
-
-  policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Effect = "Allow",
-        Action = [
-          "athena:*",
-          "s3:GetObject",
-          "s3:PutObject"
-        ],
-        Resource = "*"
-      }
-    ]
-  })
-}
-
-resource "aws_iam_access_key" "athena_key" {
-  user = aws_iam_user.athena_user.name
-}
-
-# --- Redshift User ---
-resource "aws_iam_user" "redshift_user" {
-  name = "service-user-redshift"
-}
-
-resource "aws_iam_user_policy" "redshift_policy" {
-  name = "redshift-service-policy"
-  user = aws_iam_user.redshift_user.name
-
-  policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Effect = "Allow",
-        Action = [
-          "redshift:*",
-          "logs:*"
-        ],
-        Resource = "*"
-      }
-    ]
-  })
-}
-
-resource "aws_iam_access_key" "redshift_key" {
-  user = aws_iam_user.redshift_user.name
 }
